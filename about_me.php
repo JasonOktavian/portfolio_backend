1f859d146d636477b98e62b3b887c2fd7b11d9f1
JasonOktavian <jasonoktavian30@gmail.com>
2023-03-03 11:47:11 +0700

    Adding carousel
    Adding carousel into about me

--- about_me.php
+++ about_me.php
@@ -11,6 +11,36 @@ require_once 'navbar.php';
 
 <h1>About Me</h1>
 
+<div class="container">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="img/genshin_impact___albedo_s_constellation_by_anemoenjoyer_dfczt0p.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/genshin_impact___kaedehara_kazuha_s_constellation_by_anemoenjoyer_dfczkdl.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/genshin_impact___kamisato_ayato_s_constellation_by_anemoenjoyer_dfcznl3.jpg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+</div>
+
 <div class="contents">
     <div class="row">
         <div class="col">
