36370deeb7166fb6f53f51e2117b04fc5c3833c6
JasonOktavian <jasonoktavian30@gmail.com>
2023-03-03 15:28:22 +0700

    Navbar already fixed
    Navbar and content already fixed needs to use bootstrap 4.1 not 4.0 but now live search is now not working properly

--- js/script.js
+++ js/script.js
@@ -10,21 +10,41 @@ $(document).ready(function () {
   });
 });
 
-let keyword = document.getElementById("keyword");
-let btnCari = document.getElementById("btnCari");
-let containers = document.getElementById("ajaxcon");
+// let keyword = document.getElementById("keyword");
+// let btnCari = document.getElementById("btnCari");
+// let containers = document.getElementById("ajaxcon");
 
-//tambah event ketika keyword ditulis
-keyword.addEventListener("keyup", function () {
-  //buat objek ajax
-  let xhr = new XMLHttpRequest();
+// //tambah event ketika keyword ditulis
+// keyword.addEventListener("keyup", function () {
+//   //buat objek ajax
+//   let xhr = new XMLHttpRequest();
 
-  //cek kesiapan ajax
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      containers.innerHTML = xhr.responseText;
-    }
-  };
-  xhr.open("GET", "ajax/produk.php?keyword=" + keyword.value, true);
-  xhr.send();
+//   //cek kesiapan ajax
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       containers.innerHTML = xhr.responseText;
+//     }
+//   };
+//   xhr.open("GET", "ajax/produk.php?keyword=" + keyword.value, true);
+//   xhr.send();
+// });
+
+$(document).ready(function () {
+  // hilangkan tombol cari
+  $("#btnCari").hide();
+
+  // event ketika keyword ditulis
+  $("#keyword").on("keyup", function () {
+    // munculkan icon loading
+    $(".loader").show();
+
+    // ajax menggunakan load
+    // $('#container').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+
+    // $.get()
+    $.get("ajax/produk.php?keyword=" + $("#keyword").val(), function (data) {
+      $("#container").html(data);
+      $(".loader").hide();
+    });
+  });
 });
